94f6870514da602718809ccff76553832b0743b9
Jonathan Barros <jonabf1@gmail.com>
2021-02-17 01:00:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const listGenerator = require('./lib');
 
-const source = "/home/hanma/Área de Trabalho/teste";
-const target = "/home/hanma/Área de Trabalho/alvo";
+const source = "/YourPath";
+const target = "/YourTargetToSaveList";
 
 // some extensions have exclusive formatting for final report, so far only mp3/m4a
 const especificalExtension =  ["mp3", "m4a"];
